8979867cd7b53846fa8eff47782beb8d58811609
suryanshishere <heresuryanshsingh@gmail.com>
2025-06-17 17:43:13 +0530

    redis config bug solve

--- backend/src/queue.ts
+++ backend/src/queue.ts
@@ -1,5 +1,8 @@
 import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
 
-const connection = new IORedis(process.env.REDIS_URL || 'redis://localhost:6379');
+const connection = new IORedis(process.env.REDIS_URL || 'redis://localhost:6379', {
+  maxRetriesPerRequest: null,
+});
+
 export const storeQueue = new Queue('storeQueue', { connection });
