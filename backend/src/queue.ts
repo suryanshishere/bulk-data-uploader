a8381602a26db0f487bc678f781d57aa7229cfa0
suryanshishere <heresuryanshsingh@gmail.com>
2025-06-18 17:20:45 +0530

    redis configuration setup

--- backend/src/queue.ts
+++ backend/src/queue.ts
@@ -3,7 +3,6 @@ import IORedis from "ioredis";
 
 const connection = new IORedis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null,
-  tls: {},
 });
 
 export const storeQueue = new Queue("storeQueue", { connection });
